e11b061fb19d86d3189d8e7d8d78d3cbcc5e193c
Natan Orzechowski <natan.orzechowski@nask.pl>
2021-08-21 02:18:18 +0200

    Add Operations class. Declare bytes operations methods

---
+++ src/bytes_operations.py
@@ -0,0 +1,9 @@
+class Operation:
+    def __init__(self) -> None:
+        pass
+    def xor_block(cls):
+        pass
+    def times_three(cls):
+        pass
+    def times_two(cls):
+        pass
